419dd4c885a8b7569b9a7750444ba66c9bdef2dd
Marissa Beltran <beltr077@csusm.edu>
2018-08-13 14:59:47 -0700

    base code

---
+++ main.py
@@ -0,0 +1,37 @@
+#main.py
+import webapp2
+import jinja2
+import os
+import logging
+from models import *
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        logging.info('in get self')
+        mypage = env.get_template('create_new/index.html')
+        self.response.write(mypage.render())
+    def post(self):
+        mypage = env.get_template('create_new/index.html')
+        self.response.write(mypage.render())
+
+
+class CreateNewAccPage(webapp2.RequestHandler):
+    def get(self):
+        mypage = env.get_template('template/create_new.html')
+        self.response.write(mypage.render())
+
+class LoginAccPage(webapp2.RequestHandler):
+    def get(self):
+        mypage = env.get_template('template/create_new.html')
+        self.response.write(mypage.render())
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/create_new', CreateNewAccPage),
+    ('/login', LoginAccPage)
+], debug=True)
---
+++ models.py
@@ -0,0 +1,13 @@
+#classes go here
+
+from google.appengine.ext import ndb #this is google's database
+
+#This class is for creating objects of website users
+class DuckUser(ndb.Model): #this is my constructor!
+    name = ndb.StringProperty(required = False, default="Duck Person")
+    username = ndb.StringProperty(required = True)
+    password = ndb.StringProperty(required = True)
+
+#This class is for creating....?
+class Duck(ndb.Model): #this is my constructor!
+    #duck info here
