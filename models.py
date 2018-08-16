38876c20d4eec6b8a9d10510a0fd7ff09e090fe9
Khaled <khaledwally50@hotmail.com>
2018-08-16 10:58:00 -0700

    in profilepage I added a button counter for friends

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ class DuckUser(ndb.Model): #this is my constructor!
     name = ndb.StringProperty(required = False, default="Duck Person")
     username = ndb.StringProperty(required = True)
     password = ndb.StringProperty(required = True)
+    friendCount = ndb.IntegerProperty(required = True, default=0)
 
 class Post(ndb.Model): #this is for the feed/home page!
     username = ndb.StringProperty(required = False)
